7d48db7d4db8c2e002a263dc8e7cdc7a2a8e6b3b
lzczhichao <1945815574@qq.com>
2023-03-29 18:16:13 +0000

    训练
    Signed-off-by: lzczhichao <1945815574@qq.com>

--- mytrain.py
+++ mytrain.py
@@ -3,6 +3,5 @@ from ultralytics import YOLO
 # Load a model
 model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-
 # Train the model
 model.train(data="/home/zc/下载/ultralytics-main/myfive.yaml", epochs=50, imgsz=1920,batch=6)
